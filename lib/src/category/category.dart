d1584e19662366a1e65fad8e445271bd5fd08cba
heyaphra <hey.aphra@gmail.com>
2023-08-31 16:52:58 -0700

    [concept] Category: initial commit

---
+++ lib/src/category/category.dart
@@ -0,0 +1,58 @@
+import '../morphism/morphism.dart';
+
+/// A [Category] is a mathematical structure that consists of objects and morphisms between those objects, satisfying certain axioms.
+class Category<O extends Object, M extends Morphism> {
+  // TODO: Convert objects, morphisms, and identityMorphisms from List to Set data types to ensure uniqueness.
+  // TODO: Validate morphisms to ensure the category contains id morphisms for each object - NOT generate them synthetically.
+  // TODO: Write a method to retrieve id morphisms from the morphisms list - for one or many objects.
+
+  final List<O> objects;
+  final List<M> morphisms;
+  final String? name;
+  final String? description;
+
+  Category({
+    required this.objects,
+    required this.morphisms,
+    this.name,
+    this.description,
+  });
+
+  Morphism identityMorphism(Object object) =>
+      Morphism(Transformation(object, object));
+
+  List<M> morphismsOf(Object obj) {
+    if (objects.contains(obj)) {
+      return morphisms
+          .where((morphism) => morphism.source == obj || morphism.target == obj)
+          .toList();
+    } else {
+      throw ArgumentError("Object not found in the category.");
+    }
+  }
+
+  List<M> morphismsFrom(Object obj) {
+    if (objects.contains(obj)) {
+      return morphisms.where((morphism) => morphism.source == obj).toList();
+    } else {
+      throw ArgumentError("Object not found in the category.");
+    }
+  }
+
+  List<M> morphismsTo(Object obj) {
+    if (objects.contains(obj)) {
+      return morphisms.where((morphism) => morphism.target == obj).toList();
+    } else {
+      throw ArgumentError("Object not found in the category.");
+    }
+  }
+
+  Morphism<A, C> compose<A, B, C>(Morphism<A, B> f, Morphism<B, C> g) {
+    if (f.target == g.source) {
+      return Morphism(Transformation(f.source, g.target));
+    } else {
+      throw ArgumentError(
+          "Incompatible morphism composition: source ${g.source} and target ${f.target} do not match.");
+    }
+  }
+}
